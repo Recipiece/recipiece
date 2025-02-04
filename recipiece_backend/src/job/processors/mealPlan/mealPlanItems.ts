d3600d9997c447146f2ca867a402fd8053089893
Steven Rosendahl <steven.ak.rosendahl@gmail.com>
2025-02-03 22:20:13 -0500

    good enough

--- recipiece_backend/src/job/processors/mealPlan/mealPlanItems.ts
+++ recipiece_backend/src/job/processors/mealPlan/mealPlanItems.ts
@@ -4,64 +4,69 @@ import { Job } from "bullmq";
 import { MEAT_LIKE_INGREDIENTS, processMeatThawing } from "./util";
 
 export const processMealPlanItem = async (job: Job) => {
-  return await prisma.$transaction(async (tx) => {
-    const sideJobId = job.id;
+  try {
+    return await prisma.$transaction(async (tx) => {
+      const sideJobId = job.id;
 
-    const sideJob = await tx.sideJob.findFirst({
-      where: { id: sideJobId },
-    });
+      const sideJob = await tx.sideJob.findFirst({
+        where: { id: sideJobId },
+      });
 
-    if (!sideJob) {
-      console.log(`side job ${sideJobId} not found`);
-      return undefined;
-    }
+      if (!sideJob) {
+        console.log(`side job ${sideJobId} not found`);
+        return undefined;
+      }
 
-    const jobData = YMealPlanItemJobDataSchema.cast(sideJob.job_data);
+      const jobData = YMealPlanItemJobDataSchema.cast(sideJob.job_data);
 
-    const mealPlanItem = await tx.mealPlanItem.findFirst({
-      where: {
-        id: jobData.meal_plan_item_id,
-      },
-      include: {
-        meal_plan: true,
-      },
-    });
+      const mealPlanItem = await tx.mealPlanItem.findFirst({
+        where: {
+          id: jobData.meal_plan_item_id,
+        },
+        include: {
+          meal_plan: true,
+        },
+      });
 
-    if (!mealPlanItem) {
-      console.log(`Meal Plan Item ${mealPlanItem} does not exist!`);
-      return undefined;
-    }
+      if (!mealPlanItem) {
+        console.log(`Meal Plan Item ${mealPlanItem} does not exist!`);
+        return undefined;
+      }
 
-    const config = YMealPlanConfigurationSchema.cast(mealPlanItem.meal_plan.configuration);
-    if (mealPlanItem.recipe_id) {
-      if (config.meats?.send_thawing_notification && !!config.meats.preferred_thawing_method) {
-        const meatyIngredientsQuery = tx.$kysely
-          .selectFrom("recipe_ingredients")
-          .selectAll("recipe_ingredients")
-          .leftJoin("recipes", "recipes.id", "recipe_ingredients.recipe_id")
-          .leftJoin("meal_plan_items", "meal_plan_items.recipe_id", "recipes.id")
-          .where("meal_plan_items.id", "=", mealPlanItem.id)
-          .where(() => {
-            const joined = KyselySql.raw(MEAT_LIKE_INGREDIENTS.join("|"));
-            return KyselySql`lower(recipe_ingredients.name) similar to '%(${joined})%'`;
-          });
-        const meatyIngredients = await meatyIngredientsQuery.execute();
+      const config = YMealPlanConfigurationSchema.cast(mealPlanItem.meal_plan.configuration);
+      if (mealPlanItem.recipe_id) {
+        if (config.meats?.send_thawing_notification && !!config.meats.preferred_thawing_method) {
+          const meatyIngredientsQuery = tx.$kysely
+            .selectFrom("recipe_ingredients")
+            .selectAll("recipe_ingredients")
+            .leftJoin("recipes", "recipes.id", "recipe_ingredients.recipe_id")
+            .leftJoin("meal_plan_items", "meal_plan_items.recipe_id", "recipes.id")
+            .where("meal_plan_items.id", "=", mealPlanItem.id)
+            .where(() => {
+              const joined = KyselySql.raw(MEAT_LIKE_INGREDIENTS.join("|"));
+              return KyselySql`lower(recipe_ingredients.name) ~* '(${joined})'`;
+            });
+          const meatyIngredients = await meatyIngredientsQuery.execute();
 
-        if (meatyIngredients.length > 0) {
-          console.log(`meal plan item ${mealPlanItem.id} has at least one meat like ingredient, processing thawing`);
-          const knownIngredients = await tx.knownIngredient.findMany();
-          return await processMeatThawing(tx, sideJob.user_id, mealPlanItem, config, meatyIngredients, knownIngredients);
+          if (meatyIngredients.length > 0) {
+            console.log(`meal plan item ${mealPlanItem.id} has at least one meat like ingredient, processing thawing`);
+            const knownIngredients = await tx.knownIngredient.findMany();
+            return await processMeatThawing(tx, sideJob.user_id, mealPlanItem, config, meatyIngredients, knownIngredients);
+          } else {
+            console.log(`meal plan item ${mealPlanItem.id} has no meat-like ingredients`);
+          }
         } else {
-          console.log(`meal plan item ${mealPlanItem.id} has no meat-like ingredients`);
+          console.log(
+            `configuration has send_thawing_notification=${config.meats?.send_thawing_notification} and preferred_thawing_method=${config.meats?.preferred_thawing_method}, no processing can be done.`
+          );
         }
       } else {
-        console.log(
-          `configuration has send_thawing_notification=${config.meats?.send_thawing_notification} and preferred_thawing_method=${config.meats?.preferred_thawing_method}, no processing can be done.`
-        );
+        console.log(`meal plan item ${mealPlanItem.id} has no recipe associated with it`);
       }
-    } else {
-      console.log(`meal plan item ${mealPlanItem.id} has no recipe associated with it`);
-    }
-    return undefined;
-  });
+      return undefined;
+    });
+  } catch (err) {
+    console.error(err);
+    return Promise.resolve();
+  }
 };
